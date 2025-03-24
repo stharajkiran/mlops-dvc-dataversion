3c70bc430344b4aa541c2ea07baf5fee50efabdc
Kiran Shrestha <kiranshrestha2069@gmail.com>
2025-03-24 17:26:15 -0500

    test

--- mycode.py
+++ mycode.py
@@ -8,7 +8,7 @@ data = {'Name': ['Alice', 'Bob', 'Charlie'],
     }
 
 df = pd.DataFrame(data)
-
+# rough
 # # Adding new row to df for V2
 new_row_loc = {'Name': 'GF1', 'Age': 20, 'City': 'City1'}
 df.loc[len(df.index)] = new_row_loc
